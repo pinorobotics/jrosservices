6f38978bbab53145c0d6d1df876fce522ac93423
lambdaprime <intid@protonmail.com>
2023-03-03 17:44:30 +0000

    Removing unnecessary dependency

--- jrosservices/src/main/java/module-info.java
+++ jrosservices/src/main/java/module-info.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 jrosservices project
+ *
+ * Website: https://github.com/pinorobotics/jrosservices
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * Interfaces and classes which are agnostic to ROS version of ROS Services.
  *
@@ -12,7 +29,6 @@ module jrosservices {
     // since many of our API relies on jrosclient classes we need to ensure
     // that all modules reading this module also read jrosclient
     requires transitive jrosclient;
-    requires id.xfunction;
 
     exports pinorobotics.jrosservices;
     exports pinorobotics.jrosservices.msgs;
